0cfc57f3dc0dfd0ee326d6bf28aa2349dcd9294c
Aleks Saul <aleks.saul@gmail.com>
2016-09-05 22:30:23 -0400

    minor changes

--- scripts/quay/dockerfile_build_completed.sh
+++ scripts/quay/dockerfile_build_completed.sh
@@ -12,5 +12,13 @@ exec &> >(tee -a "$logfile")
 echo `date` - Executing $0 
 
 echo $HOOK_PAYLOAD > $statusdir/test/quay_build_completed.json
+Github_Branch=$(echo $HOOK_PAYLOAD | jq '.trigger_metadata.ref' | tr -d '"' | awk 'BEGIN { FS = "/"} ; {print $3}')
+if [ ! -f $statusdir/$Github_Branch/github_pr_open.json ] ; then
+	echo `date` - ERROR: $statusdir/$Github_Branch/github_pr_open.json does not exist!
+	exit 1;
+fi
+
 
 #$(echo $HOOK_PAYLOAD |jq '.error_message')
+
+echo `date` - Done executing $0
